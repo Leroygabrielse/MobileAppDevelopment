9e42cac94efd61df0e9e116b0bd475e86810305d
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-03-19 21:22:31 +0100

    commands lvl1ex
    commands lvl1ex

--- Level1Example/app/src/main/java/com/example/leroygabrielse/picturequiz/MainActivity.kt
+++ Level1Example/app/src/main/java/com/example/leroygabrielse/picturequiz/MainActivity.kt
@@ -1,26 +1,40 @@
+/*
+Applicatie Level 1 Example
+Deze applicatie laat de gebruiker een giraf zien,
+Vervolgens moet hij in een veld invoeren wat voor dier het is.
+
+Gemaakt door: Leroy Gabriëlse
+Student nr.: 500761062
+e-mail: leroy.gabrielse@hva.nl
+
+*/
+
 package com.example.leroygabrielse.picturequiz
 
+//library imports
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    //Methode die wordt uitgevoard bij startup van de app
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Methode die een klik op de knop registreert
         button.setOnClickListener{
             checkAnswer()
         }
 
     }
 
-    //Checkt of het antwoord klopt
+    //Methode die invoer vergelijkt met 'giraffe' en feedback op het antwoord geeft met een toast.
     private fun checkAnswer(){
-        val answer = etAnswer.text.toString()
+        val answer = etAnswer.text.toString().toLowerCase()
 
         if (answer == getString(R.string.giraffe)){
             Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
