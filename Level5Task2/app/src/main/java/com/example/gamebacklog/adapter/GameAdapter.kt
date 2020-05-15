96833cde89299b2ea9f20b43994b9d374cc22650
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-05-15 21:00:40 +0200

    rv en adapter

---
+++ Level5Task2/app/src/main/java/com/example/gamebacklog/adapter/GameAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.gamebacklog.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gamebacklog.R
+import com.example.gamebacklog.model.Game
+import kotlinx.android.synthetic.main.game_item.view.*
+
+class GameAdapter(private val games: List<Game>) : RecyclerView.Adapter<GameAdapter.NoteViewHolder>() {
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(game: Game){
+            itemView.tvGameName.text = game.title
+            itemView.tvPlatform.text = game.platforms
+            itemView.tvReleaseDate.text = game.releaseDate.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.game_item, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = games.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(games[position])
+    }
+}
---
+++ Level5Task2/app/src/main/java/com/example/gamebacklog/model/Game.kt
@@ -0,0 +1,17 @@
+package com.example.gamebacklog.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Parcelize
+@Entity
+data class Game (
+    var title: String,
+    var platforms: String,
+    var releaseDate: Date,
+    @PrimaryKey var id: Long? = null
+) : Parcelable
