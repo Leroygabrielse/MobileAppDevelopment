6f570952c6639398ee8f2b4e0d81b2b5786e5b8f
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-06-01 20:55:30 +0200

    layout fixes, fully working

--- Level6Task1/app/src/main/java/com/example/advancedrecview/ColorItemRepo.kt
+++ Level6Task1/app/src/main/java/com/example/advancedrecview/ColorItemRepo.kt
@@ -6,7 +6,7 @@ class ColorItemRepo {
             ColorItem("000000", "Black"),
             ColorItem("FF0000", "Red"),
             ColorItem("FFFF00", "Yellow"),
-            ColorItem("000800", "Green"),
+            ColorItem("5DA271", "Green"),
             ColorItem("E3E3E3", "Whitish")
         )
     }
